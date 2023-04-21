3c27c3ee311cf4f42ae692d1abc6f7f0805c0ba0
HanSolo <mitto.hai.7356@gmail.com>
2023-04-21 22:37:09 +0700

    firtcommit

---
+++ frontend/web/index.php
@@ -0,0 +1,21 @@
+<?php
+if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'We don\'t have mysqli!!!';
+} else {
+    echo 'Phew we have it!';
+}
+$servername = "10.5.0.3";
+$username = "root";
+$password = "root";
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+echo phpinfo();
+
+
+// Create connection
+
+?>
